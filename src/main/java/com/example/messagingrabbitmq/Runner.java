27662a8a876329133d350fbb4e932103f0c36df9
Michael Gallinago <michael.gallinago@gmail.com>
2024-05-07 18:41:16 +0500

    Done

---
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -0,0 +1,34 @@
+package com.example.messagingrabbitmq;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Write your message:");
+            String message = scanner.nextLine();
+            rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "foo.bar.baz", message);
+            receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        }
+    }
+}
